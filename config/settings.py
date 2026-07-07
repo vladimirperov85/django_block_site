70636e657062510ca84f4736261f06afcacdac00
vladimirperov85 <vova.perov.85@gmail.com>
2026-07-07 13:16:44 +0300

    несущественные изменения в файлах.

--- config/settings.py
+++ config/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql', # post
-        'NAME': os.getenv('DB_NAME'),
-        'USER':os.getenv('DB_USER'),
-        'PASSWORD' :os.getenv('DB_PASSWORD') ,
-        'HOST':os.getenv('DB_HOST','localhost'),
-        'PORT':os.getenv('DB_PORT','5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql', # post
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER':os.getenv('DB_USER'),
+#         'PASSWORD' :os.getenv('DB_PASSWORD') ,
+#         'HOST':os.getenv('DB_HOST','localhost'),
+#         'PORT':os.getenv('DB_PORT','5432'),
+#     }
+# }
 
 
 
